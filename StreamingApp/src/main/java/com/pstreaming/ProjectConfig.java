d6d6c5045c919a5979d0f3a9823871f28bc701eb
Rafael Solano S. <rafaelsolanosilv@gmail.com>
2025-09-14 21:29:58 -0600

    Update 14 de septiembre
    Se realiza el login mediante goole y se agrega la config para darle los permisos al usuario

--- StreamingApp/src/main/java/com/pstreaming/ProjectConfig.java
+++ StreamingApp/src/main/java/com/pstreaming/ProjectConfig.java
@@ -1,8 +1,9 @@
 package com.pstreaming;
 
+import com.pstreaming.controller.OAuth2LoginSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,9 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 @EnableWebSecurity
 public class ProjectConfig {
 
+    @Autowired
+    private OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+
     @Bean
     public PasswordEncoder aEncoder() {
         return new BCryptPasswordEncoder();
@@ -22,12 +26,20 @@ public class ProjectConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/usuario/registro", "/usuario/login", "/index").permitAll()
+                .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/usuario/registro", "/usuario/login", "/index", "/").permitAll()
+                .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
-            )
-            .oauth2Login(Customizer.withDefaults());
-
+                )
+                .oauth2Login(oauth2 -> oauth2
+                .loginPage("/usuario/login")
+                .successHandler(oAuth2LoginSuccessHandler)
+                )
+                .formLogin(form -> form
+                .loginPage("/usuario/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/dashboard")
+                );
         return http.build();
     }
 
